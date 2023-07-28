b926eaef3664fa447d0b111c4f86b6b7750c105c
nicfournier <33913566+nicfournier@users.noreply.github.com>
2023-07-28 13:25:16 -0400

    structure

--- app.config.ts
+++ app.config.ts
@@ -1,41 +1,58 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'CX Orford',
+    description: 'Championnat québécois 2023 de cyclocross et finale de la Coupe Québec',
+
     image: {
-      src: '/social-card-preview.png',
+      src: '/cx-orford-header.png',
       alt: 'An image showcasing my project.',
       width: 400,
       height: 300
     },
+
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
+        path: '/cx-orford-header.png', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
         alt: 'alpine' // alt of the logo
       }
     },
+
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: false,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: 'CX Orford'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: 'Suivez nous' // string that will be displayed in the footer (leave empty or delete to disable)
     },
+
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: '',
+      instagram: 'https://www.instagram.com/classiqueasimon/',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      facebook: 'https://www.facebook.com/people/La-Classique-%C3%A0-Simon/100092620497035/'
     },
+
     form: {
       successMessage: 'Message sent. Thank you!'
+    },
+
+    backToTop: {
+      text: 'Retour au sommet'
     }
   }
 })
